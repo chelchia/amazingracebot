0aa5bff2f1447145760ae0c55a5251df4fe3e8e9
chelchia <chelseachiajh98@gmail.com>
2018-05-30 11:12:35 +0800

    Cleaned up comments.

--- commands.py
+++ commands.py
@@ -3,20 +3,12 @@ from telegram.ext import CommandHandler
 from qrcodeHandler import qrcodeHandler
 
 # ===== Bot commands ===== 
-
 # /start
 def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="Started chat with amazing race bot.")
 
 # /station
 def station(bot, update):
-	# bot.send_message(chat_id=update.message.chat_id, text="Send me a pic of the qr code. Text 'cancel' to cancel.")
-	# reply = bot.get_updates() #PROBLEM: get updates too soon, doesnt get the correct update
-	# while (true):
-	# 	reply = bot.get_updates()
-	# 	if (reply != []):
-	# 		break
-	# qrcodeHandler(bot, reply)
 	forceReplyObj = telegram.ForceReply(force_reply=True, selective=False)
 	bot.send_message(chat_id=update.message.chat_id, text="Send me a pic of the qr code.", reply_markup=forceReplyObj)
 
--- settings.py
+++ settings.py
@@ -1,9 +1,6 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-#dotenv_path = join(dirname(__file__), '.env')
-#load_dotenv(dotenv_path)
-#load_dotenv()
 load_dotenv(find_dotenv())
 
 token = os.environ.get("TOKEN")
