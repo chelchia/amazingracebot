70578c6e8b7ca9cd8b41eafe7bf20749af5481d0
chelchia <chelseachiajh98@gmail.com>
2018-07-09 11:10:22 +0800

    Modified settings to retrieve token from heroku config

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+# from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+# load_dotenv(find_dotenv())
 
-token = os.environ.get("TOKEN")
+# token = os.environ.get("TOKEN")
+token = os.environ['TELEGRAM_TOKEN']
+
 
